ad5425b207af1c8c509e3b7a043b8038b90719bc
Ashwwar7318 <ashwarsrivastava7318@gmail.com>
2026-07-26 13:06:17 +0530

    Swap_of_var_using_friend_function
    Example of friend function

---
+++ Swap_of_var_using_friend_function.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+class c2;
+
+class c1
+{
+    int val1;
+    friend void exchange(c1 x, c2 y);
+
+public:
+    int indata(int a)
+    {
+        val1 = a;
+    }
+    int display(void)
+    {
+        cout << val1 << endl;
+    }
+};
+
+class c2
+{
+    int val2;
+    friend void exchange(c1 x, c2 y);
+
+public:
+    int indata(int a)
+    {
+        val2 = a;
+    }
+    int display(void)
+    {
+        cout << val2 << endl;
+    }
+};
+
+void exchange(c1 x, c2 y)
+{
+    int temp = x.val1;
+    x.val1 = y.val2;
+    y.val2 = temp;
+}
+int main()
+{
+    c1 oc1;
+    c2 oc2;
+    oc1.indata(34);
+    oc2.indata(67);
+    exchange(oc1, oc2);
+
+    cout << "The value of oc1 after exchange becomes: ";
+    oc1.display();
+
+    cout << "The value of oc2 after exchang becomes: ";
+    oc2.display();
+
+    return 0;
+}
